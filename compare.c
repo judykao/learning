25ca9397ee2c24fc64b55f1a7eddc136a4f62d2b
Judy Kao <imjudykao@gmail.com>
2018-06-11 22:20:57 +0800

    second version of compare

--- compare.c
+++ compare.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
-int max(int a,int b){
+int max(int a,int b) {
+  scanf("");
+  int c;
+  if(a>b) c=a;
+  else c=b;
 
-int c;
-if(a>b) c=a;
-else c=b;
-
-return c;
-}
-int main()
-{
-int a=20;
-int b=30;
-int e,d;
-
-e=max(a,b);//first compare
-
-a=30;
-b=40;
-
-d=max(a,b);
-
-printf("第一次比較結果為%d\n第二次比較結果為%d\n",e,d);
-   
+  return c;
 }
 
+int main() {
+
+  int a=20;
+  int b=30;
+  int e,d;
+
+  e=max(a,b);//first compare
+
+  a=30;
+  b=40;
+
+  d=max(a,b);
+
+  printf("第一次比較結果為%d\n第二次比較結果為%d\n",e,d);
+
+}
